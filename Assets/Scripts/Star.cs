46b45187ea4cc7ba7889f28b461b845ea6f03da0
Michael Boyle <mbbexley@hotmail.co.uk>
2020-11-03 10:37:47 +0000

    Add starfield and stars, plus constellation

---
+++ Assets/Scripts/Star.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Star : MonoBehaviour
+{
+    [SerializeField]
+    private Transform centreSphere;
+
+    [SerializeField, Min(0f)]
+    private float projectDistance = 1000f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 direction = transform.position - centreSphere.position;
+        transform.position += direction.normalized * projectDistance;
+        transform.localScale *= projectDistance * 0.2f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Starfield.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfield : MonoBehaviour {
+    [SerializeField]
+    private Transform startLocation;
+
+    [SerializeField]
+    private int starCount = 5000;
+
+    [SerializeField]
+    private float starDistanceMin = 2000f, starDistanceMax = 4000f;
+
+    [SerializeField]
+    private GameObject star;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < starCount; i++) {
+            float dist = Random.Range(starDistanceMin, starDistanceMax);
+            Vector3 location = (Random.onUnitSphere * dist) + startLocation.position;
+            GameObject s = Instantiate(star, location, Quaternion.identity, transform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
