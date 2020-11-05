440ff1211c786b275e0b2691b25150c3d947ee24
Michael Boyle <mbbexley@hotmail.co.uk>
2020-11-05 19:55:25 +0000

    Make a pose for gripping the magnet, and make the magnet an actual object

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        // We know this is the goal in this game, but let's make sure
+        if (other.transform.name == "Goal") {
+
+        }
+    }
+}
